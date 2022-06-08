18194850ef81b27362602c3605c830055ef0940e
Md-sahil-a <mdsahilamaan2017@gmail.com>
2022-06-08 22:13:43 +0530

    Sass & Iamges added

--- clothing-data/src/App.js
+++ clothing-data/src/App.js
@@ -3,47 +3,49 @@ import './style.scss'
 function App() {
   const productData = [
     {
-      id: "1",
-      title: "Hats",
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: "2",
-      title: "Shoes",
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: "3",
-      title: "Jackets",
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: "4",
-      title: "Mens",
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: "5",
-      title: "Womens",
-    },
-
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
   return (
     <div className="categories-container">
     {
-      productData.map(({title,id}) =>{
-        return <div className="check">
-        <div key={id} className="category-container" >
-        <div className="category-body-container">
-        <h1>
-        {title}
-        </h1>
-        <p>
-        Shop Now
-        </p>
-        </div>
-        </div>
+      productData.map(({title,id, imageUrl}) =>(
         
+        <div key={id} className="category-container">
+        <div className="background-image" style={{
+          backgroundImage: `url(${imageUrl})`
+        }} >
         </div>
-      })
+        <div className="category-body-container">
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+        </div>
+        </div>
+  
+      ))
     }
-     
     </div>
   );
 }
