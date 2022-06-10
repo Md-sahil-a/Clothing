b7eb408196e7918f2f7f7bb00c3cc63bd847d4e8
Md-sahil-a <mdsahilamaan2017@gmail.com>
2022-06-10 20:47:12 +0530

    Routes issue fixed

--- clothing-data/src/App.js
+++ clothing-data/src/App.js
@@ -1,10 +1,21 @@
 
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/routescomponents/homeroute/home.component";
+
+function Shop(){
+  return (
+    <h1>
+    THis is Shop pAGE
+    </h1>
+  )
+}
+
+
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path='/shop' element={<Shop/>} />
     </Routes>
   )
 }
