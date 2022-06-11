effd79a65ebc528a24508d6fc27b77172c65e948
Md-sahil-a <mdsahilamaan2017@gmail.com>
2022-06-11 13:12:03 +0530

    Working on Navigation

--- clothing-data/src/App.js
+++ clothing-data/src/App.js
@@ -1,6 +1,21 @@
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from "./components/routescomponents/homeroute/home.component";
+
+
+
+const Nav = ()=>{
+  return(
+    <div>
+    <div>
+    <h1>
+    This is Navigation bar.
+    </h1>
+    </div>
+    <Outlet/>
+    </div>
+  )
+}
 
 function Shop(){
   return (
@@ -14,7 +29,8 @@ function Shop(){
 function App() {
   return (
     <Routes>
-    <Route path="/home" element={<Home />} >
+    <Route path='/' element={<Nav/>}>
+      <Route index element={<Home/>} />
       <Route path='shop' element={<Shop />}/>
     </Route>
     </Routes>
