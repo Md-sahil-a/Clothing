fa00791598b99672386ebd5795462b9d38fba27a
Md-sahil-a <mdsahilamaan2017@gmail.com>
2022-06-14 17:36:37 +0530

    Sign up Form set

--- clothing-data/src/components/Signin-with-mail/signinwithform.jsx
+++ clothing-data/src/components/Signin-with-mail/signinwithform.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react"
 
+const defaultFormFields = {
+    displayName: "",
+    email: "",
+    password: "",
+    ConfirmPassword:""
+}
 
 const EmailLSignup = ()=>{
+    const [Fields,setFields ] = useState(defaultFormFields);
+    const {displayName, email, password, ConfirmPassword} = Fields;
+    console.log(Fields);
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+        setFields({...Fields, [name]: value});
+    }
+
     return (
-        <form>
-        <label>Name</label>
-        <input type="name" required ></input>
+        <div>
+        <h1>Sign UP with Email and Password</h1>
+        <form onSubmit={()=>{}}>
+        <label>DisplayName</label>
+        <input type="name" required  onChange={handleChange} name ="displayName" value={displayName}/>
         <label>Email</label>
-        <input type="email"  required></input>
-        <label>PassWord</label>
-        <input type="password" required ></input>
-        <button type="onSubmit">Submit</button>
+        <input type="email" required onChange={handleChange} name="email" value={email} />
+        <label>Password</label>
+        <input type="password" required onChange={handleChange} name="password" value={password}  />
+        <label>Confirm Password</label>
+        <input type ="password" required onChange={handleChange} name="ConfirmPassword" value={ConfirmPassword} />
+        <button>Submit</button>
         </form>
+        </div>
     )
 }
 
