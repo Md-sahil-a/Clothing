d2be55e165bc3ab1a6e03560cc9912cb37802230
Md Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2022-07-13 20:27:20 +0530

    Reacting with react

--- clothing-data/src/App.js
+++ clothing-data/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/routescomponents/homeroute/home.component";
 import Nav from "./components/routescomponents/NavRoute/nav";
 import Sign from "./components/routescomponents/NavRoute/signin/signin";
 import Shop from "./components/shop-data/shopdata";
-//contexts
+
 
 function App() {
   return (
